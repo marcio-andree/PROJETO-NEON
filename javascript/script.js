6a13fb9d2aaad326da7ac469f5dc650850efc346
Marcio André <andremarcio1988@gmail.com>
2023-02-02 10:28:10 -0300

    add javascript , styled section s-depoimentos

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+
+    // Swiper library
+
+    var swiper = new Swiper(".slide-depoimentos", {
+      slidesPerView: 3,
+      spaceBetween: 32,
+      pagination: {
+        el: ".sliderPaginationDepoimento",
+        clickable: true,
+      },
+    });
+
