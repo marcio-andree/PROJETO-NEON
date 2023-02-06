91c5ed64ad9c978feb1678549a5b9bd09de3a171
Marcio André <andremarcio1988@gmail.com>
2023-02-06 16:00:22 -0300

    responsive s-depoimentos

--- javascript/script.js
+++ javascript/script.js
@@ -8,5 +8,19 @@
         el: ".sliderPaginationDepoimento",
         clickable: true,
       },
+      breakpoints: {
+        320: {
+          slidesPerView: 1.3,
+          spaceBetween: 16,
+        },
+        600: {
+          slidesPerView: 2.3,
+          spaceBetween: 15,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        },
+      },
     });
 
