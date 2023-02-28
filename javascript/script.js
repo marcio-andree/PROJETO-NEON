4e8e2a6551abea572b556f792f166fc3384e9313
Marcio André <andremarcio1988@gmail.com>
2023-02-28 10:46:18 -0300

    menu dropdown

--- javascript/script.js
+++ javascript/script.js
@@ -28,3 +28,18 @@
       once:true
     });
 
+
+
+    const btnDropdown = document.querySelector('.js-btn-dropdown');
+
+    const dropdown = document.querySelector('.js-dropdown');
+
+    function openDropdown(event){
+      event.preventDefault();
+
+      dropdown.classList.toggle('active')
+    }
+
+    btnDropdown.addEventListener('click', openDropdown);
+
+    dropdown.addEventListener('mouseleave', openDropdown);
